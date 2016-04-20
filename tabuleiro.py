969e1189799365ea6b1628ce14bb63419b107a9c
King-Bits <bvbitelli@gmail.com>
2016-04-20 11:43:37 -0300

    xablau

---
+++ tabuleiro.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 11:34:04 2016
+
+@author: bvbit
+"""
+
+import jogo
+
+class Tabuleiro:
+    
+    def _init_ (self):
+        self.jogo_tabuleiro = jogo.Jogo()
+                
