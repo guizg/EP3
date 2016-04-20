b838121ba999ee1e3d0f7b88cb4ab9f94ee93aeb
Guilherme Zaborowsky Graicer <Guilherme Zaborowsky Graicer>
2016-04-20 11:49:41 -0300

    fazendootrabalhodo beit el

--- tabuleiro.py
+++ tabuleiro.py
@@ -6,9 +6,13 @@ Created on Wed Apr 20 11:34:04 2016
 """
 
 import jogo
+import tkinter as tk
 
 class Tabuleiro:
     
     def _init_ (self):
         self.jogo_tabuleiro = jogo.Jogo()
+        self.window = tk.Tk()
+        self.window.title("Jogo da Velha")
+        
                 
