4a2027739e47e2f82951d50cdf623d9754399c99
Guilherme Zaborowsky Graicer <Guilherme Zaborowsky Graicer>
2016-04-25 23:31:57 -0300

    comentei o jogo.py

--- jogo.py
+++ jogo.py
@@ -7,24 +7,24 @@ Created on Wed Apr 20 10:01:39 2016
 
 import numpy as np
 
-class Jogo:
+class Jogo: # simula um jogo da velha
     
     def __init__(self):
-        self.jogador = 1
-        self.rep_tabuleiro = np.zeros([3,3])
+        self.jogador = 1 # 1 = X e 2 = O
+        self.rep_tabuleiro = np.zeros([3,3]) # Cria uma matriz de zeros 3x3 que vai representar o tabuleiro do jogo
 
-    def recebe_jogada(self,linha,coluna):
+    def recebe_jogada(self,linha,coluna): # troca o numero na matriz pelo numero do jogador e altera o jogador
         self.rep_tabuleiro[linha][coluna] = self.jogador
         if self.jogador == 1:
             self.jogador = 2
         else:
             self.jogador = 1
             
-    def limpa_jogadas(self):
+    def limpa_jogadas(self): # reseta o jogo
         self.rep_tabuleiro = np.zeros([3,3])
         self.jogador = 1
     
-    def verifica_ganhador(self):
+    def verifica_ganhador(self): # checa se o jogo acabou. retorna 1 se o jogador 1 ganhar, 2 se o 2 ganhar, 0 se der velha, e -1 se o jogo nao tiver acabado
         for i in range(3): # checa as vitorias de linha inteira
             if self.rep_tabuleiro[i][0] == 1 and self.rep_tabuleiro[i][1]==1 and self.rep_tabuleiro[i][2]==1:
                 return 1
